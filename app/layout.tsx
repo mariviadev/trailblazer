3c97b69c7a8909a6c7fc4ad71b8588572fb42b0e
Mariam Khan <52761858+mariam1203@users.noreply.github.com>
2024-06-03 01:28:21 -0400

    Project Setup Done

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export const metadata: Metadata = {
+  title: "Trailblazer",
+  description: "Travel UI/UX App for Camping",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
