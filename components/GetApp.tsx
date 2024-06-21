a8cb5140c262fd86f230850b8afa10d40b135c2d
Mariam Khan <52761858+mariviadev@users.noreply.github.com>
2024-06-21 03:05:29 -0400

    GetApp and Footer Finished
    - finished whole site following JSM
    - pending my updates plus some visual UI fixes

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import Button from "./Button";
+import Image from "next/image";
 
 const GetApp = () => {
   return (
-    <div>GetApp</div>
-  )
-}
+    <section className="flexCenter w-full flex-col pb-[100px]">
+      <div className="get-app">
+        {/* LEFT SIDE */}
+        <div className="z-20 flex flex-col flex-1 w-full items-start justify-center gap-12">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">
+            Get for free now!
+          </h2>
+          <p className="regular-16 text-gray-10">
+            Available on iOS and Android
+          </p>
 
-export default GetApp
+          <div className="flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row">
+            <Button
+              type="button"
+              title="Download on App Store"
+              icon="/apple.svg"
+              variant="btn_white"
+              full
+            />
+            <Button
+              type="button"
+              title="Download on Play Store"
+              icon="/android.svg"
+              variant="btn_dark_green_outline"
+              full
+            />
+          </div>
+        </div>
+
+        {/* RIGHT SIDE */}
+        <div className="flex flex-1 items-center justify-end">
+          <Image src="/phones.png" alt="phones" width={550} height={870} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GetApp;
