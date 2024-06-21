9db9a392f64eae68e25b308ddc2b1d789f413e7f
Mariam Khan <52761858+mariviadev@users.noreply.github.com>
2024-06-21 00:59:26 -0400

    Hero and Camp Sections Finished
    - finished hero and camp sections following JSM
    - did some of my own changes as I followed along (check out TODO and TODONE comments)

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,14 +4,14 @@ type ButtonProps = {
   type: "button" | "submit";
   title: string;
   icon?: string;
-  variant: "btn_dark_green";
+  variant: string;
 };
 
 const Button = ({ type, title, icon, variant }: ButtonProps) => {
   return (
     <button type={type} className={`flexCenter gap-3 rounded-full border ${variant}`}>
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <label className="bold-16 whitespace-nowrap">{title}</label>
+      <span className="bold-16 whitespace-nowrap">{title}</span>
     </button>
   );
 };
